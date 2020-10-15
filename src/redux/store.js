50e29132ad916b04eb9d949edcf91adca8991208
Shalitha <shalithax@gmail.com>
2020-10-15 12:19:44 +0530

    changeViews

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import reducers from './reducers';
-import thunk from 'redux-thunk';
-
-const store = createStore(reducers, applyMiddleware(thunk));
-
-export default store;
